485ab22714e884837a56eeb0e33989f56ab9fe8e
Gabriel Machado <g_machado1@hotmail.com>
2019-04-13 11:41:24 -0300

    Implementado model e view de mensagem

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,12 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._mensagem = new Mensagem();
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._mensagemView = new MensagemView($('#mensagemView'));
 
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
@@ -17,6 +20,8 @@ class NegociacaoController {
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
